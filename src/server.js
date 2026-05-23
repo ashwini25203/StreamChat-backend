997d7873d92a710ea671104596fed17b710cc651
ashwini25203 <ashwinikhairnar63@gmail.com>
2026-05-23 21:04:18 +0530

    cors policy changed

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const __dirname = path.resolve();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://stream-chat-frontend-amber.vercel.app",
     credentials: true, // allow frontend to send cookies
   })
 );
