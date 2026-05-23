2735dea2d8ee9df03749bef0545932d4db979f63
ashwini25203 <ashwinikhairnar63@gmail.com>
2026-05-23 20:38:39 +0530

    first commit

---
+++ src/routes/user.route.js
@@ -0,0 +1,32 @@
+import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import {
+  acceptFriendRequest,
+  getFriendRequests,
+  getMyFriends,
+  getOutgoingFriendReqs,
+  getRecommendedUsers,
+  sendFriendRequest,
+} from "../controllers/user.controller.js";
+// routes/user.routes.js
+
+import { getMyProfile } from "../controllers/user.controller.js";
+
+// 👇 Add this route
+
+const router = express.Router();
+
+// apply auth middleware to all routes
+router.use(protectRoute);
+
+router.get("/", getRecommendedUsers);
+router.get("/friends", getMyFriends);
+router.get("/me", getMyProfile);
+
+router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+
+export default router;
