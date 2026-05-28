c405e692b56ecbc65de008676b11adbb3d2d7924
ashwini25203 <ashwinikhairnar63@gmail.com>
2026-05-28 21:44:20 +0530

    logout code changed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -96,10 +96,18 @@ export async function login(req, res) {
   }
 }
 
-export function logout(req, res) {
-  res.clearCookie("jwt");
-  res.status(200).json({ success: true, message: "Logout successful" });
-}
+export const logout = (req, res) => {
+  res.clearCookie("jwt", {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "none",
+  });
+
+  res.status(200).json({
+    success: true,
+    message: "Logout successful",
+  });
+};
 
 export async function onboard(req, res) {
   try {
